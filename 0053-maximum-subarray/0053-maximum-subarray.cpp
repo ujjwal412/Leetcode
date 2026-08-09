3fd87685343be619c5aa68ee040ae5a1bcbd930a
ujjwal412 <ujjwal.kumar.cs23@ggits.net>
2026-08-09 23:52:43 +0530

    Time: 3 ms (18.72%), Space: 71.9 MB (15.16%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      int bestEnding = nums[0];
+      int ans = nums[0];
+      for(int i=1; i<nums.size(); i++){
+        int v1 = bestEnding + nums[i];
+        int v2 = nums[i];
+        bestEnding = max(v1,v2);
+        ans = max(ans,bestEnding);
+      }
+      return ans;
+    }
+};
