76b9f1de0d7584ff80979ac5fc095b4184a79521
ujjwal412 <ujjwal.kumar.cs23@ggits.net>
2026-08-21 23:08:57 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (53.09%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty())
+            return 0;
+       int res = 1;
+       for(int i=1;i<nums.size(); i++){
+        if(nums[i]!=nums[i-1]){
+           nums[res] = nums[i];
+           res++;
+        }
+        else{
+            continue;
+        }
+
+       }  
+       return res;   
+    }
+};
