2ce8bdfd5be205b5e0af0fbfdbb67979a4b6f5ca
ujjwal412 <ujjwal.kumar.cs23@ggits.net>
2026-08-24 17:59:49 +0530

    Time: 321 ms (16.85%) | Memory: 60.6 MB (13.81%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        if(s.size()<k)
+        return 0;
+        unordered_map<char, int> freq;
+
+        for(char c: s){
+            freq[c]++;
+        }
+        for(int i=0; i<s.size(); i++){
+           if(freq[s[i]]<k){
+            int left=longestSubstring(s.substr(0, i), k);
+            int right = longestSubstring(s.substr(i + 1), k);
+
+            return max(left,right);
+           }
+        }
+        return s.size();
+    }
+};
